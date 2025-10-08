37e15d0120d01e8648b2425c2314b1c338d167f4
Kau7almeida <kau7almeida.06@gmail.com>
2025-10-08 17:45:41 -0300

    font family atualizada

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,12 +2,16 @@ import Styles from './Navbar.module.css'
 
 import { Link } from 'react-router-dom'
 
+import logo from '../../assets/Layer_1.svg'
+import logo2 from '../../assets/Logo 02.svg'
+
 export default function Navbar() {
     return (
         <header>
             <div className={Styles.container}>
                 <div className={Styles.title_header}>
                     <p className={Styles.title_jynx}>Universidade JYNX</p>
+                    {/* <img src={logo} alt="" /> */}
                     <p className={Styles.barra}>|</p>
                     <p className={Styles.cargo}>Instrutor</p>
                 </div>
